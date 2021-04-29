bc166e91a6731642a3935f72da718599acd46a6b
tuckerTU <tuckerishere@gmail.com>
2021-04-29 09:41:38 -0600

    commit tree edacafac0b111ea6848ce3e6f7bbd93da6cc6067

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do 
+        "My name is Tucker."
+    end
+
+    get '/hometown' do 
+        "My hometown is New Orleans."
+    end
+
+    get '/favorite-song' do 
+        "My favorite song is Hey Ya."
+    end
+
 end
